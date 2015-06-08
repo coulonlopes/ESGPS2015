7996449bf66e58bb66835ec8d6a877c6c44df9e1
carlasjgomes <carla.sofia373@hotmail.com>
2015-06-08 16:07:13 +0100

    lmkl

--- Health/AdicionarConsulta.cs
+++ Health/AdicionarConsulta.cs
@@ -14,6 +14,10 @@ namespace Health
     public partial class AdicionarConsulta : Form
     {
         //isto trabalha????
+        //
+        //
+        //
+        //
         int sns;
         public AdicionarConsulta()
         {
